240d3105690363edd40c899187f6a615d20cfcd9
Dany9966 <you@example.com>
2018-04-16 11:10:24 +0300

    tar achive recognition and trace deletion + TODO commments

--- assignment1.sh
+++ assignment1.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#TODO tarCheck function
+
 
 #if tar archive 
 tarCheck=`echo $1 | grep -Ec '(*\.tar\.gz$|*\.tgz$)'`
@@ -27,11 +29,13 @@ for i in $filelist
 do
     #echo $i
     if test -d $i ; then
-	#echo "directory $i"
-	#$0 $i 
-	continue
-	
-    elif test -f $i ; then #if regfile then check if there is occurance
+		#echo "directory $i"
+		$0 $i 
+		#continue
+		
+    elif test -f $i ; then #if regfile then check if tar archive TODO
+
+    	#else check occurance
 		check=`cat $i | grep -c 'much Open, such Stack'`
 		if test $check -gt 0 ; then #if there are, print filenames and count
 		    echo $i
@@ -47,7 +51,7 @@ if test $archive -eq 1 ; then
 		rm -rf $i
 	done
 fi
-
-
+#if last argument is 1 then return value TODO
 return $count
+#else print final count TODO
 echo "there are $count occurences"
